41f62a4a70069fa7d3307987054a73f1fbd3ff09
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2021-01-14 23:58:33 -0500

    [ADD]: Add Text Section and apply CSS

--- src/components/countrieDetail/CountrieDetail.js
+++ src/components/countrieDetail/CountrieDetail.js
@@ -3,13 +3,13 @@ import { useParams } from 'react-router-dom';
 import { useCountries } from '../../providers/CountrieProvider';
 import {FaArrowLeft} from 'react-icons/fa';
 import './CountrieDetail.css';
+import { useTheme } from '../../providers/ThemeColorProvider';
 
 const CountrieDetail = ({history}) => {
   
   const { id } = useParams();
-
   const { countries } = useCountries();
-
+  const { theme } = useTheme();
   const countrie = useMemo(() => countries.find(currentCountrie => currentCountrie.alpha2Code === id), [id, countries]);
 
   if(!countrie){
@@ -18,6 +18,7 @@ const CountrieDetail = ({history}) => {
 
   const  {
     flag,
+    name,
     nativeName, 
     population,
     region, 
@@ -45,7 +46,46 @@ const CountrieDetail = ({history}) => {
       </button>
       <div className="flag-text-container">
         <img className="img-flag" src={flag} alt=""/>
-        
+        <div className={`text-section  ${theme ? "dark-text" : "light-text"}`}>
+          <h2>{ name }</h2>
+
+          <div className='information-text'>
+            <div className='left-section-text'>
+              <p>
+                Native Name: <span>{ nativeName }</span>
+              </p>
+              <p>
+                Population: <span>{ population }</span>
+              </p>
+              <p>
+                Region: <span>{ region }</span>
+              </p>
+              <p>
+                Sub Region: <span>{ subregion }</span>
+              </p>
+              <p>
+                Capital: <span>{ capital }</span>
+              </p>
+            </div>
+            <div className="right-section-text">
+              <p>
+                Top Level Domain: <span>FIX</span>
+              </p>
+              <p>
+                Currencies: <span>FIX</span>
+              </p>
+              <p>
+                Languages: <span>FIX</span>
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <span>Border Countries:</span>
+          </div>
+
+          
+        </div>
       </div>
     </div>
   )
