3eb9ccf7f80c43355a6d04cfb08b4f2b7121623b
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2021-01-16 23:55:00 -0500

    [ADD]: Endpoint to get all Countries Added

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,24 @@
 import axios from 'axios';
 
+const baseURL = 'https://restcountries.eu/rest/v2';
+
+export async function getAllCountries(){
+
+  const URL = `${baseURL}/all`;
+  
+  try {
+    const { data: countries } = await axios.get(URL);
+    console.log(countries);
+    return countries;
+
+  } catch (error) {
+    console.log("🚀 ~ file: index.js ~ line 13 ~ getCountriesByContinent ~ error", error)
+  }
+}
+
 export async function getCountriesByContinent(continent = 'europe'){
 
-  const URL = `https://restcountries.eu/rest/v2/region/${continent}`;
+  const URL = `${baseURL}/region/${continent}`;
   
   try {
     const { data: countries } = await axios.get(URL);
@@ -12,3 +28,21 @@ export async function getCountriesByContinent(continent = 'europe'){
     console.log("🚀 ~ file: index.js ~ line 13 ~ getCountriesByContinent ~ error", error)
   }
 }
+
+export async function getCountrieByCode(code){
+
+  if(!code) return;
+
+  const URL =  `${baseURL}/alpha/${code}`;
+  
+  try {
+    
+    const { data: countrie} = await axios.get(URL);
+    
+    return countrie;
+
+  } catch (error) {
+    console.log(error);
+  }
+
+} 
